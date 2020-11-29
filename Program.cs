50fb75a43d35ceadcb29e54ba3a0fbb0c85e64f6
Nikita <valodko99@inbox.ru>
2020-11-29 14:49:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Utils.ConsoleInit();
+            Utils.InitGraph();
+            
+            for (int row = 1; row <= Utils.ScanSizeX; row++)
+            {
+                Utils.RigthPanel.SetActive(row);
+
+                int coll = 1;
+                bool ToRight = true;
+                int prevLine = 1;
+
+                while(true)
+                {
+                    Utils.LinePainter.VerticalLines.VerticalClearLine(prevLine);                    
+                    Utils.LinePainter.VerticalLines.VerticalLineTo(coll);
+                    
+                    Thread.Sleep(3);
+
+                    prevLine = coll;
+
+                    if (ToRight)
+                    {
+                        coll++;
+                    }
+                    else
+                    {
+                        coll--;
+                    }
+
+                    if(coll == Utils.ScanSizeY - 1)
+                    {
+                        ToRight = false;
+                    }
+                    else if(coll == 1)
+                    {                        
+                        ToRight = true;
+                    }
+                }
+            }
+
+
+            /*
+            for(int a = 0; a < 3000; a++)
+            {
+                Utils.RadarScanCycle();
+                Thread.Sleep(7);
+            }*/
+
+
+
+
+
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleApp1
+{
+    enum TargetSize
+    {
+        size1,
+        size2,
+        size3
+    }
+
+    class Target
+    {
+        public byte X;
+        public byte Y;
+        public TargetSize Size;
+    }
+
+
+    static class Utils
+    {
+        /// <summary>
+        /// Область сканирования по Х
+        /// </summary>
+        public static int ScanSizeX;
+
+        /// <summary>
+        /// Размер сканирования по Y
+        /// </summary>
+        public static int ScanSizeY;
+
+        public static TextWriter ConsoleWriter = Console.Out;
+        public static int prevLineX = 1;
+
+        /// <summary>
+        /// Установка параметров консоли
+        /// </summary>
+        public static void ConsoleInit()
+        {
+            ScanSizeX = 40;
+            ScanSizeY = 96;
+
+            Console.SetWindowSize(100, 43);
+            Console.CursorVisible = false;
+        }
+        
+        public static void InitGraph()
+        {
+            StreamReader Reader = new StreamReader("Carcas.txt");
+
+            while (!Reader.EndOfStream)
+            {
+                Console.WriteLine(Reader.ReadLine());
+            }
+
+            Reader.Close();
+        }
+
+
+        public static class LinePainter
+        {
+            public static class VerticalLines
+            {
+                /// <summary>
+                /// Поставить линию в колонку
+                /// </summary>
+                /// <param name="x">Номер колонки</param>
+                /// <param name="startPos">С какой начать рисовать</param>
+                /// <param name="endPos">До какой рисовать</param>
+                public static void VerticalLineTo(int x, int startPos = 1, int endPos = 42)
+                {
+                    for (int y = startPos; y < endPos; y++)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write("|");
+                    }
+
+                    prevLineX = x;
+                }
+
+                /// <summary>
+                /// Стереть линию с колонки
+                /// </summary>
+                /// <param name="x"></param>
+                /// <param name="startPos">С какой начать стирать</param>
+                /// <param name="endPos">До какой стирать</param>
+                public static void VerticalClearLine(int x, int startPos = 1, int endPos = 42)
+                {
+                    for (int y = startPos; y < endPos; y++)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Out.Write(" ");
+                    }
+                }
+            }
+
+            public static class HorizontalLines
+            {
+                public static void HorizontalLineTo(int y, int startPos = 1, int endPos = 99)
+                {
+                    Console.SetCursorPosition(startPos, y);
+
+                    for (int x = startPos; x < endPos; x++)
+                    {
+                        Console.Out.Write("-");
+                    }
+                }
+
+                public static void HorizontalClearLine(int y, int startPos = 1, int endPos = 99)
+                {
+                    Console.SetCursorPosition(startPos, y);
+
+                    for (int x = startPos; x < endPos; x++)
+                    {
+                        Console.Out.Write(" ");
+                    }
+                }
+            }
+
+        }
+
+        public static class RigthPanel
+        {
+            public static string Symb = "<";
+            private static int PanelY = 97;
+            private static int prevRow = 0;
+
+            public static void SetActive(int row, bool clear = true)
+            {
+                if(clear && prevRow != 0)
+                {
+                    Console.SetCursorPosition(PanelY, prevRow);
+                    Console.Write(" ");
+                }
+
+                Console.SetCursorPosition(PanelY, row);
+                Console.Write(Symb);
+
+                prevRow = row;
+            }
+
+            public static void Clear(int row)
+            {
+                Console.SetCursorPosition(PanelY, row);
+                Console.Write(Symb);
+            }
+        }
+
+
+        private static int ActiveScanX = 4;
+        private static int ActiveScanY = 4;
+
+        public static void RadarScanCycle()
+        {
+            /**********************Проверки*********************/
+            if(ActiveScanX <= 1)
+            {
+                ActiveScanX = 2;
+            }
+
+            if(ActiveScanY <= 1)
+            {
+                ActiveScanY = 2;
+            }
+            
+            if(ActiveScanX == Console.WindowWidth - 1)
+            {
+                ActiveScanX = 2;
+            }
+
+            if (ActiveScanY == Console.WindowHeight - 1)
+            {
+                ActiveScanY = 2;
+            }
+
+            /**************РЕНДЕР*******************/
+            LinePainter.VerticalLines.VerticalClearLine(prevLineX);
+            LinePainter.VerticalLines.VerticalLineTo(ActiveScanX);
+
+            ActiveScanX++;
+        }
+    }
+}
